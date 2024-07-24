2dda1f7998780c59f6bd88ddb5255d795eb8bde6
Alex <ak@AKs-MacBook-Pro.local>
2024-07-24 02:05:02 -0700

    Added Adjacent Issue local hosting

---
+++ The Emperor’s New Headset_ What’s Wrong With VR – ADJACENT Issue 1_files/api.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/* PLEASE DO NOT COPY AND PASTE THIS CODE. */(function(){var CFG='___grecaptcha_cfg';if(!window[CFG]){window[CFG]={};}var GR='grecaptcha';if(!window[GR]){window[GR]={};}window[GR].ready=window[GR].ready||function(f){(window[CFG]['fns']=window[CFG]['fns']||[]).push(f);};(window[CFG]['fallback']=window[CFG]['fallback']||[]).push('true');(window[CFG]['render']=window[CFG]['render']||[]).push('onload');window['__google_recaptcha_client']=true;var po=document.createElement('script');po.type='text/javascript';po.async=true;po.src='https://web.archive.org/web/20181213025327/https://www.gstatic.com/recaptcha/api2/v1543818755456/recaptcha__en.js';var elem=document.querySelector('script[nonce]');var n=elem&&(elem['nonce']||elem.getAttribute('nonce'));if(n){po.setAttribute('nonce',n);}var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(po, s);})();
+
+}
+/*
+     FILE ARCHIVED ON 02:53:27 Dec 13, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 05:52:50 Jul 24, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.823
+  exclusion.robots: 0.043
+  exclusion.robots.policy: 0.025
+  esindex: 0.022
+  cdx.remote: 49.057
+  LoadShardBlock: 1007.12 (3)
+  PetaboxLoader3.datanode: 565.168 (5)
+  PetaboxLoader3.resolve: 905.11 (3)
+  load_resource: 582.463 (2)
+*/
